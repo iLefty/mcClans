3bec1078046fc4db922db44a6158b625271902f2
Articuno <admin@pixelmon.pro>
2017-08-12 16:16:25 +0300

    Update Messages.java

--- src/main/java/nl/riebie/mcclans/messages/Messages.java
+++ src/main/java/nl/riebie/mcclans/messages/Messages.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 riebie, Kippers <https://bitbucket.org/Kippers/mcclans-core-sponge>
+ * Copyright (c) 2017 riebie, Kippers <https://bitbucket.org/Kippers/mcclans-core-sponge>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
